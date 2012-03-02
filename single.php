ef5d9d2cf6efe7e610ae0daad7708c4ebd310427
landryon <landryon@119bc55c-a502-428b-ae15-d6966b898115>
2012-03-02 04:36:34 +0000

    Cleaned up unecessary <div> and some code.

--- single.php
+++ single.php
@@ -9,20 +9,24 @@
 
 get_header(); ?>
 <!--single.php-->
-<div class="nav-banner">
 		<div id="primary" width="100%">
 			<div id="content" role="main">
 
-				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
+
+					<nav id="nav-single">
+						<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
+						<span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
+						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
+					</nav><!-- #nav-single -->
+
 					<?php get_template_part( 'content', 'single' ); ?>
 
 					<?php comments_template( '', true ); ?>
 
 				<?php endwhile; // end of the loop. ?>
 
-			</div>
-		</div>
+			</div><!-- #content -->
+		</div><!-- #primary -->
 <?php get_sidebar(); ?>
-</div>
 <?php get_footer(); ?>
